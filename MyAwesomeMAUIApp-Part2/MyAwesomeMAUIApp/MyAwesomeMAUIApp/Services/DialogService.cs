beef5e863dd1f6ccd8580f14bde477047b6453d1
Sebastian Jensen <apps@tsjdev-apps.de>
2022-05-26 20:34:14 +0200

    Add sample project for .NET MAUI - Part 2.

---
+++ MyAwesomeMAUIApp-Part2/MyAwesomeMAUIApp/MyAwesomeMAUIApp/Services/DialogService.cs
@@ -0,0 +1,13 @@
+﻿using MyAwesomeMAUIApp.Resources.Strings;
+
+namespace MyAwesomeMAUIApp.Services
+{
+    public class DialogService : IDialogService
+    {
+        public async Task ShowAsync(string title, string message)
+        {
+            await Application.Current.MainPage?
+                .DisplayAlert(title, message, AppResources.DialogOkLabel);
+        }
+    }
+}
